69ec2ba669ce77533595e0985034c04154d346f9
abdul ghani <mohammedghani842@gmail.com>
2021-09-20 12:43:46 -0700

    P3

---
+++ problem3.js
@@ -0,0 +1,59 @@
+// Write a function which addUser to the user array only if the user does not exist.
+const users = [
+    {
+        name:'Brook', 
+        scores:75,
+        skills:['HTM', 'CSS', 'JS'],
+        age:16
+    },
+    {
+        name:'Alex', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    }, 
+    {
+        name:'David', 
+        scores:75,
+        skills:['HTM', 'CSS'],
+        age:22
+    }, 
+    {
+        name:'John', 
+        scores:85,
+        skills:['HTM'],
+        age:25
+    },
+    {
+        name:'Sara',
+        scores:95,
+        skills:['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name:'Martha', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    },
+    {
+        name:'Thomas',
+        scores:90,
+        skills:['HTM', 'CSS', 'JS'],
+        age:20
+    }
+    ];
+
+    function addUser(user){
+        if(users.find(users => users.name === user.name)==null)
+         //getting result of user by name who has similar name to new added user.... if this is not true then new object will be pushed into array of objects
+        users.push(user);
+    }
+    let user = {
+        name: "Ghani",
+        scores: 85,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 20
+       }
+    addUser(user);
+    console.table(users)
