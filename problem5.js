defe1ab55d64cffd8a97579fdca85bc4e9e90918
abdul ghani <mohammedghani842@gmail.com>
2021-09-20 12:51:17 -0700

    P5

---
+++ problem5.js
@@ -0,0 +1,60 @@
+// Write a function which editUser if the user exist in the users array.
+  
+const users = [
+    {
+        name:'Brook', 
+        scores:75,
+        skills:['HTM', 'CSS', 'JS'],
+        age:16
+    },
+    {
+        name:'Alex', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    }, 
+    {
+        name:'David', 
+        scores:75,
+        skills:['HTM', 'CSS'],
+        age:22
+    }, 
+    {
+        name:'John', 
+        scores:85,
+        skills:['HTM'],
+        age:25
+    },
+    {
+        name:'Sara',
+        scores:95,
+        skills:['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name:'Martha', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    },
+    {
+        name:'Thomas',
+        scores:90,
+        skills:['HTM', 'CSS', 'JS'],
+        age:20
+    }
+    ];
+
+
+    
+    function editUser( name, newName ) {
+        for (var i in users) {
+          if (users[i].name == name) {
+             users[i].name = newName;
+             break;
+          }
+        }
+     }
+
+    editUser('Thomas','Abdul Ghani'); //john replaced with santosh
+    console.table(users)
